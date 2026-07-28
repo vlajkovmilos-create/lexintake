5730521c51bf9cc0a68524c8a6f8bbbcde57aa85
Miloš Vlajkov <vlajkovmilos@icloud.com>
2026-07-28 16:21:29 +0200

    Preci na PostgreSQL sa psycopg2

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,11 +4,14 @@ from .config import get_settings
 
 settings = get_settings()
 
-engine = create_engine(
-    settings.database_url,
-    connect_args={"check_same_thread": False}
-)
-
+if settings.database_url.startswith("sqlite"):
+    engine = create_engine(
+        settings.database_url,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(settings.database_url)
+    
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
